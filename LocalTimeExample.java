41d61ac96c6d40d4b0c9e33fe19aa854a4e5fe91
Aaron Rykhus <aprykhus@hotmail.com>
2020-08-11 14:46:17 -0500

    Initial commit

---
+++ LocalTimeExample.java
@@ -0,0 +1,30 @@
+// Windows 10 - JavaSE 14
+
+import java.lang.Thread;
+import java.time.LocalTime;
+
+class LocalTimeExample {
+	
+	public static void cls() {
+		try {
+			new ProcessBuilder("cmd", "/c","cls").inheritIO().start().waitFor();
+		} catch(Exception E) {
+			System.out.println(E);
+		}
+	}
+	
+	public static void display(int hour, int min, int sec){
+		cls();
+		System.out.printf("%d:%d:%d%n", hour, min, sec);
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		LocalTime thisSec;
+
+		for (;;) {
+			thisSec = LocalTime.now();
+			display(thisSec.getHour(), thisSec.getMinute(), thisSec.getSecond());
+			Thread.sleep(1000);
+		}
+	}
+}
